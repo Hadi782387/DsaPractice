9be54383859b3ddc7abe2d9c6f097dca508129ab
Yaseen <siddiquiyaseen641@gmail.com>
2025-07-01 11:58:06 +0530

    java practice exercises for logic Building

--- src/PrimeFactorization.java
+++ src/PrimeFactorization.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
+
 public class PrimeFactorization {
     public static void main(String[] args) {
-        int n = 32;
+        java.util.Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
         for(int div = 2; div < n;div++){
             while(n%div==0){
                 n/=div;
