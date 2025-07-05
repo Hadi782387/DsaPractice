74ccfb5c91c026ee80c7d7846acc1d2f4afc0f63
Yaseen <siddiquiyaseen641@gmail.com>
2025-07-05 12:39:10 +0530

    java practice exercises for logic Building

---
+++ src/SpanOfArray.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class SpanOfArray {
+    public static void main(String[] args) {
+        Scanner s  = new Scanner(System.in);
+        int n = s.nextInt();
+        int [] arr = new int[n];              // step 1: creating a array size of n
+
+        for (int i =0;i<arr.length;i++){       // step 2: taking input elements of array
+            arr[i]=s.nextInt();
+        }
+        int max = arr[0];
+        int min = arr[0];
+        for (int i =0;i<arr.length;i++){
+            if (arr[i]>max) {                //step 3 if i is greater than max
+                max=arr[i];
+            }
+            if (arr[i]<min){                 //step 4: if i is smaller than min
+                min=arr[i];
+            }
+
+        }
+        int span = max-min;                  //step 5; formula of span
+        System.out.println(span);
+    }
+}
