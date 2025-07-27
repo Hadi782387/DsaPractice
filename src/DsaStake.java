d6f0304d32242f36c0008bbdbbc9c0f92ca8e8b5
Yaseen <siddiquiyaseen641@gmail.com>
2025-07-27 09:48:48 +0530

    Fix: added support for uppercase vowels in project (#1)

---
+++ src/DsaStake.java
@@ -0,0 +1,11 @@
+import java.util.Stack;
+
+public class DsaStake {
+    public static void main(String[] args) {
+        Stack<Integer> st = new Stack<>();
+        st.push(23);
+        st.push(23);
+        st.push(13);
+        System.out.println(st);
+    }
+}
