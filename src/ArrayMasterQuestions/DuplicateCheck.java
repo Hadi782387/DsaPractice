a5cabd635a2ac6db82526139e941c3a499e87876
Yaseen <siddiquiyaseen641@gmail.com>
2025-08-14 09:44:58 +0530

    i resolve my issue

--- src/ArrayMasterQuestions/DuplicateCheck.java
+++ src/ArrayMasterQuestions/DuplicateCheck.java
@@ -1,24 +1,34 @@
 package ArrayMasterQuestions;
-// in that question there is an array with 5 elements check if there is ant duplicate
+
 import java.util.Scanner;
 
 public class DuplicateCheck {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
-        int [] arr = new int[n];
-        if (n==0){
+        int[] arr = new int[n];
+
+        if (n == 0) {
             System.out.println("False");
             return;
         }
-        for (int i =0;i<arr.length;i++){
-            arr[i]=s.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = s.nextInt();
         }
-        int data=s.nextInt();
-        for (int i:arr){
-            if (data==arr[i]){
-                System.out.println("");
+
+        boolean hasDuplicate = false;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] == arr[j]) {
+                    hasDuplicate = true;
+                    break;
+                }
             }
+            if (hasDuplicate) break;
         }
+
+        System.out.println(hasDuplicate ? "True" : "False");
     }
 }
